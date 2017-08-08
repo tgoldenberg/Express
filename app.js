058b002be0e2d94cbbf69d258f6ba4b4a2d0d151
Thomas Goldenberg <thomasagoldenberg@gmail.com>
2017-08-08 15:57:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 const express = require('express')
 
+const Methods = require('./methods');
 const app = express()
 
+async function getUsers(req, res) {
+  try {
+    let users = await request(url, options);
+    if (!!users) {
+      res.status(200).send({ users: users });
+    } else {
+      res.status(500).send({ error: 'No users!' });
+    }
+  } catch (e) {
+    console.warn(e);
+    res.status(500).send({ error: 'Unable to make request' });
+  }
+}
+
+async function createUser(req, res) {
+ try {
+   let newUser = await request(url, options);
+   if (!!newUser) {
+     res.status(200).send({ user: newUser });
+   } else {
+     res.status(500).send({ error: 'Could not save new user.' });
+ } catch (e) {
+   console.warn(e);
+   res.status(500).send({ error: 'Could not save new user.' });
+ }
+}
+
+const Methods = {
+  getUsers: getUsers,
+  createUser: createUser,
+};
+
 // GET method route
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage')
-})
+app.get('/v1/users', Methods.getUsers);
 
 // POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
-})
+app.post('/', Methods.createUser)
 
 app.get('/ab+cd', function (req, res) {
   res.send('Hello world I am abcd!')
